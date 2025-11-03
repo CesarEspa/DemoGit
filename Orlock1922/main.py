2850db922c060e20632d21003c4a572a1229990a
carlosoo7 <Carlosfer047@gmail.com>
2025-11-02 20:08:59 -0500

    JSON musica

---
+++ Orlock1922/main.py
@@ -0,0 +1,11 @@
+estudiante = {
+    "nombre" : "Paul Thomas",
+    "edad" : 28,
+    "cursos" : ["Matematicas", "Fisica", "Programacion"],
+    "promedio" : 4.2,
+    "correo" : "restrictedsecret@hormail.com",
+    "nota" : 4.5,
+    "musica": ["classic", "rock", "EDM"]
+}
+
+print(estudiante["musica"])
